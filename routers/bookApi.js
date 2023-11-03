16a883cded7747c79e48abeb50e39bae484b0b3b
atheo810 <atheos810@gmail.com>
2023-11-03 17:16:08 +0800

    feat: API findMany

--- routers/bookApi.js
+++ routers/bookApi.js
@@ -2,8 +2,13 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/api/book", (req, res) =>
-  res.status(200).json("Selamat Datang di Api Book")
-);
+const BookApi = require("../controllers/bookApiController");
+
+const { getBook } = BookApi;
+
+// router.get("/api/book", (req, res) =>
+//   res.status(200).json("Selamat Datang di Api Book")
+// );
+router.get("/api/book", getBook);
 
 module.exports = router;

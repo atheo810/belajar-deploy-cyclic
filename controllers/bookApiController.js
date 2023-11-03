0595dbff488340f6eb46d681c50ecc300da1cb8b
atheo810 <atheos810@gmail.com>
2023-11-03 17:15:47 +0800

    feat: module Export Api

--- controllers/bookApiController.js
+++ controllers/bookApiController.js
@@ -7,4 +7,18 @@ class BookApiController {
 
     res.status(200).json(result);
   }
+
+  static async getDetailBook(req, res) {
+    const result = await prisma.book.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+
+    if (result === null) {
+      res.status(404).json({ message: "Data not found" });
+    } else {
+      res.status(200).json(result);
+    }
+  }
 }
+
+module.exports = BookApiController;

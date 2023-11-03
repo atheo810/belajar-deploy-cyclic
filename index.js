ead4b775022ba4f03f8c9eb6d30998a71ed66404
atheo810 <atheos810@gmail.com>
2023-11-03 13:39:26 +0800

    feat: use Router

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const router = require("./routers");
+
+app.use(router);
+
 app.get("/", (req, res) => res.send("hello World"));
 
 app.listen(3000);

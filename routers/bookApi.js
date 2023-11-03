75707671addc69a38e4665d39da6feb8dcaed581
atheo810 <atheos810@gmail.com>
2023-11-03 19:19:42 +0800

    feat: getDetailBook

--- routers/bookApi.js
+++ routers/bookApi.js
@@ -4,11 +4,12 @@ const router = Router();
 
 const BookApi = require("../controllers/bookApiController");
 
-const { getBook } = BookApi;
+const { getBook, getDetailBook } = BookApi;
 
 // router.get("/api/book", (req, res) =>
 //   res.status(200).json("Selamat Datang di Api Book")
 // );
 router.get("/api/book", getBook);
+router.get("/api/book/:id", getDetailBook);
 
 module.exports = router;
